b174c437ac942534873411084dee6f41b75a6c95
diksha-kalra <kalradiksha11@gmail.com>
2020-10-02 20:15:57 +0530

    Added: given null return happy

--- test/java/com/moodanalyser/MoodAnalyserTest.java
+++ test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -10,6 +10,20 @@ public class MoodAnalyserTest {
 		String mood=moodAnalyser.analyseMood();
 		Assert.assertEquals("SAD",mood);
 	}
+	
+	@Test
+	public void giveMessage_WhenNotSad_ShouldReturnHappy() {
+       		MoodAnalyser moodAnalyser = new MoodAnalyser("This is a Happy Message");
+       		String mood=moodAnalyser.analyseMood();
+       		Assert.assertEquals("HAPPY",mood);
+   }
+
+	@Test
+	 public void givenNullMood_ShouldReturnHappy() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser(null);	
+		String mood = moodAnalyser.analyseMood();
+		Assert.assertEquals("HAPPY",mood);
+	 }
 
 }	
 	
